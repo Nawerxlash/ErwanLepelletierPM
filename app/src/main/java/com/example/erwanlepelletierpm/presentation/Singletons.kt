f9243051c757c05d5c547ab9bb603f0176de1226
Nawerxlash <erwan.lepelletier@lepaje.fr>
2021-05-24 17:11:19 +0200

    Fin du projet + fin cache

--- app/src/main/java/com/example/erwanlepelletierpm/presentation/Singletons.kt
+++ app/src/main/java/com/example/erwanlepelletierpm/presentation/Singletons.kt
@@ -1,11 +1,20 @@
 package com.example.erwanlepelletierpm.presentation
 
 import com.example.erwanlepelletierpm.presentation.api.YugiApi
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class Singletons {
     companion object {
+
+        /*var cache = Cache(File(context?.cacheDir,"responses"),10 * 1024 * 1024)
+
+        val okHttpClient: OkHttpClient = OkHttpClient().newBuilder()
+            .cache(cache)
+            .build()*/
+
          val yugiApi: YugiApi = Retrofit.Builder()
                 .baseUrl("https://db.ygoprodeck.com/api/v7/")
                 .addConverterFactory(GsonConverterFactory.create())
